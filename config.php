14492d39845d6cfdac2c5d37816725df01733019
Mateus Augusto Tozin Ferreira <mateustozin15@gmail.com>
2022-11-03 16:36:49 -0300

    PHP_cadastro_PJBL

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+    $dbHost = 'Localhost'
+    $dbUsername = 'root'
+    $dbPassword = ''
+    $dbName = 'cadastro'
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName)
+
+    //if($conexao->connect_errno)
+    //{
+    //echo"Erro";
+    //}
+
+    //else
+    //{
+    //echo"Conexao ok "
+    //}
+
+
+?>
+
+
